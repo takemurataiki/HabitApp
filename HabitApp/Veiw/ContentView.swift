90174b4cf54076644b13d55abb80084e86537e99
竹村太喜 <taitake204@gmail.com>
2021-01-29 08:20:05 +0900

    あ

--- HabitApp/Veiw/ContentView.swift
+++ HabitApp/Veiw/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         VStack {
             Spacer()
             Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-                Text("習慣追加ボタン")
+                Text("習慣追加ボタンだよ")
             }
             Spacer()
         
