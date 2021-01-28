6b9e9908c84c6b2bcd2a2e89b76f86fae979f985
竹村太喜 <taitake204@gmail.com>
2021-01-28 15:52:42 +0900

    Initial Commit

---
+++ HabitApp/HabitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitAppApp.swift
+//  HabitApp
+//
+//  Created by 竹村　太喜 on 2021/01/26.
+//
+
+import SwiftUI
+
+@main
+struct HabitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitApp/Veiw/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HabitApp
+//
+//  Created by 竹村太喜 on 2021/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
